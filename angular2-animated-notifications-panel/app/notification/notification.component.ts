3cdc333b59dca1f7e953e40f7be282a6c4ed7e8a
Sam Storie <storie@gmail.com>
2016-07-23 16:30:46 -0500

    Updates and additional comments to support the animations blog post

--- angular2-animated-notifications-panel/app/notification/notification.component.ts
+++ angular2-animated-notifications-panel/app/notification/notification.component.ts
@@ -1,11 +1,7 @@
 import {
     Component,
     OnInit,
-    OnChanges,
-    SimpleChanges,
     Input,
-    Output,
-    EventEmitter,
     trigger,
     state,
     style,
@@ -21,34 +17,28 @@ import {Notification, NotificationType} from "../shared/index";
     templateUrl: 'notification.component.html',
     styleUrls: ['notification.component.css'],
     animations: [
+        // Define an animation that adjusts the opactiy when a new item is created
+        //  in the DOM. We use the 'visible' string as the hard-coded value in the 
+        //  trigger.
+        //
+        // When an item is added we wait for 300ms, and then increase the opacity to 1
+        //  over a 200ms time interval. When the item is removed we don't delay anything
+        //  and use a 200ms interval.
+        //
         trigger('visibleTrigger', [
             state('visible', style({ opacity: '1' })),
-            transition('void => *', [
-                style({ opacity: '0' }),
-                animate('200ms 300ms')
-            ]),
-            transition('* => void', [
-                animate('200ms', style({ opacity: '0' }))
-            ])
+            transition('void => *', [style({ opacity: '0' }), animate('200ms 300ms')]),
+            transition('* => void', [animate('200ms', style({ opacity: '0' }))])
         ])
     ]
 })
-export class NotificationComponent implements OnInit, OnChanges {
+export class NotificationComponent implements OnInit {
     @Input() notification: Notification;
     @Input() expanded: boolean;
-    @Output() remove = new EventEmitter<any>(false);
-
-    expandedState = 'collapsed';
 
     notificationTypes = NotificationType;
 
     constructor() { }
 
     ngOnInit() { }
-
-    ngOnChanges(changes: SimpleChanges) {
-        if (changes["expanded"] !== undefined) {
-            this.expandedState = this.expanded ? 'expanded' : 'collapsed';
-        }
-    }
 }
