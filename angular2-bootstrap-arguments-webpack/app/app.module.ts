ea8bfe80ebf66b9a893178ea1e7131a8a7b7b9c7
Sam Storie <storie@gmail.com>
2016-08-13 06:38:10 -0500

    Got the example working using the browser 'window' object

--- angular2-bootstrap-arguments-webpack/app/app.module.ts
+++ angular2-bootstrap-arguments-webpack/app/app.module.ts
@@ -3,14 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
 
-export function buildAppModule(apiUrl: string, apiToken: string) {
-
 @NgModule({
   imports: [ BrowserModule ],
-  providers: [provide("api.config", { useValue: { apiUrl: apiUrl, apiToken: apiToken} })],
   declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  bootstrap:    [ AppComponent ],
+  //
+  // Here's where we access the data set by the server in the original
+  //  index.html page. We can take advantage of the factory method of
+  //  creating a provider, and use the anonymous function to access the
+  //  data off of the 'window' object at run-time.
+  //
+  providers: [{provide: "api.config", useFactory: () => window['appdata']}],
 })
 export class AppModule { }
-
-}
